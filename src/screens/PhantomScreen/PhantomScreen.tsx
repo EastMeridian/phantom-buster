500e0673b5859d8b6b17fa32030701571a8b0232
gbeillard <gbeillard@bimandco.com>
2024-06-03 16:09:31 +0200

    update PhantomScreen.tsx

--- src/screens/PhantomScreen/PhantomScreen.tsx
+++ src/screens/PhantomScreen/PhantomScreen.tsx
@@ -5,12 +5,29 @@ const PhantomScreen = () => {
   const params = useParams();
   const { phantoms } = usePhantoms();
 
-  const phantom = phantoms.find(({ id }) => params.id);
+  const phantom = phantoms.find(({ id }) => id === params.id);
+  if (!phantom) return null;
+
+  const { name, nextLaunchIn, repeatedLaunchTimes, launchType } = phantom;
 
   console.log(phantom, params);
+
+  const launchFrenquency = repeatedLaunchTimes
+    ? repeatedLaunchTimes.simplePreset
+    : launchType;
+
   return (
     <div className="bg-background flex flex-col pt-12 min-h-screen px-8">
-      salut
+      <h1 className="text-3xl text-primaryText font-bold mb-8">{name}</h1>
+      <div className="flex gap-2 items-center">
+        <div className="text-secondaryText text-xs">{launchFrenquency}</div>
+        {nextLaunchIn && (
+          <div className="text-primaryText text-sm opacity-50">|</div>
+        )}
+        {nextLaunchIn && (
+          <div className="text-primaryText text-xs">{launchFrenquency}</div>
+        )}
+      </div>
     </div>
   );
 };
